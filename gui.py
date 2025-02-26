e93678c637c8db1426200d623c331f0a079f583c
CoderDude2 <isaacboots1@gmail.com>
2025-02-26 11:50:56 -0800

    Added create_machine_file function

--- gui.py
+++ gui.py
@@ -6,6 +6,43 @@ from collections import namedtuple
 BASE_DIR: Path = Path(__file__).resolve().parent
 MachineData = namedtuple("MachineData", "machine pg_id")
 
+FOOTER_TEXT = """
+M2
+M99
+
+
+$2
+
+M2
+M99
+
+$0
+A2-LE-2-20-12-P-M
+#814=0000014000
+#815=0000005000
+#816=0000001000
+#817=0002500000
+#822=0000000010
+#824=-000001000
+#818=0000060000
+#819=0000001000
+#918=0000000000
+#821=0000000000
+#921=0000000000
+#919=0000000000
+#922=0000000000
+#990=0004136000
+#991=0000055000
+#992=0000067000
+#893=0000000000
+#25974=0004050000
+#25975=0004058000
+#25976=0004058000
+#25977=0000000000
+#25978=0000000000
+#25979=0000000000
+#25980=0000000000
+%"""
 
 class App(tk.Tk):
     def __init__(self) -> None:
@@ -65,7 +102,28 @@ class App(tk.Tk):
                 else:
                     machines[machine_data.machine].append(machine_data.pg_id)
 
-        print(machines)
+        self.cnc_data_textarea.delete('1.0', 'end')
+        for k,v in machines.items():
+            self.create_machine_file(k, v)
+    
+    def create_machine_file(self, machine: str, pg_ids: list[str]) -> None:
+        machine_file_path:Path = BASE_DIR.joinpath(f"output/1{int(machine)}.prg")
+        machine_file_path.touch()
+
+        header: str = (f"O1{int(machine)}(FOR INPUT           )\n$1\n")
+        with machine_file_path.open('w+') as file:
+            file.write(header)
+
+            for i in range(4):
+                file.write(f'#50{i+1}=\nG4 U0.5\n')
+
+            num: int = 505
+            for pg_id in pg_ids:
+                file.write(f'#{num}={pg_id}\nG4 U0.5\n')
+                num += 1
+            
+            file.write(FOOTER_TEXT)
+
 
     def is_valid(self, line_text: str) -> bool:
         if re.match(r"\s+[\n]?", line_text) or line_text == "":
